8ce15db22cbc491e8b2cbacbfcd3a7ddd6a42bce
cauecalil <caue.26@hotmail.com>
2026-03-26 23:09:06 -0300

    refactor(infra): enforce immutable ECR tags and update lifecycle policy

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,19 +2,20 @@ module "ecr" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "3.2.0"
 
-  repository_name         = var.repository_name
-  repository_force_delete = true
+  repository_name                 = var.repository_name
+  repository_force_delete         = true
+  repository_image_tag_mutability = "IMMUTABLE"
 
-  create_lifecycle_policy           = true
+  create_lifecycle_policy = true
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last 30 images",
+        description  = "Keep last 20 images",
         selection = {
-          tagStatus     = "any",
-          countType     = "imageCountMoreThan",
-          countNumber   = 30
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 20
         },
         action = {
           type = "expire"
