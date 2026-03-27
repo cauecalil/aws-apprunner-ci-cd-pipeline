b50ed5483861bb9c143a5356b5f41426bcb7bb10
cauecalil <caue.26@hotmail.com>
2026-03-26 23:09:46 -0300

    refactor(infra): simplify AppRunner config to use public image only

--- terraform/modules/apprunner/main.tf
+++ terraform/modules/apprunner/main.tf
@@ -1,7 +1,3 @@
-locals {
-  is_public_image = startswith(var.app_image_uri, "public.ecr.aws")
-}
-
 module "apprunner" {
   source  = "terraform-aws-modules/app-runner/aws"
   version = "1.2.2"
@@ -11,23 +7,14 @@ module "apprunner" {
   source_configuration = {
     auto_deployments_enabled = false
 
-    authentication_configuration = local.is_public_image ? {} : {
-      access_role_arn = var.access_role_arn
-    }
-
     image_repository = {
-      image_identifier      = var.app_image_uri
-      image_repository_type = local.is_public_image ? "ECR_PUBLIC" : "ECR"
+      image_identifier      = "public.ecr.aws/aws-containers/hello-app-runner:latest"
+      image_repository_type = "ECR_PUBLIC"
 
       image_configuration = {
-        port = tostring(var.container_port)
-
+        port                          = var.container_port
         runtime_environment_variables = var.environment_variables
       }
     }
-  }
-
-  instance_configuration = {
-    instance_role_arn = var.instance_role_arn
   }
 }
--- terraform/modules/apprunner/variables.tf
+++ terraform/modules/apprunner/variables.tf
@@ -3,23 +3,10 @@ variable "service_name" {
 }
 
 variable "container_port" {
-  type = number
+  type = string
 }
 
 variable "environment_variables" {
   type    = map(string)
   default = {}
-}
-
-variable "instance_role_arn" {
-  type = string
-}
-
-variable "access_role_arn" {
-  type = string
-}
-
-variable "app_image_uri" {
-  type    = string
-  default = "public.ecr.aws/aws-containers/hello-app-runner:latest"
 }
